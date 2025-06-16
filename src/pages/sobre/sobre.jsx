9ecdf9644e20e9888c0e406fdc466f509577338c
Rodrigo Schuab <rodschuab@gmail.com>
2025-06-16 16:12:41 -0300

    Atualizacao no css e adicionando fotos e descricoes restantes

--- src/pages/sobre/sobre.jsx
+++ src/pages/sobre/sobre.jsx
@@ -8,13 +8,13 @@ const Grupo3 = [
   {
     nome: 'Cauã Pacheco',
     imagem: '/imagens/caua.jpg',
-    descricao: '.',
+    descricao: 'Sou Cauã Pacheco um estudante de Análise e Desenvolvimento de Sistemas (ADS) e desenvolvedor full-stack em formação. Me dedico a aprender e aplicar conhecimentos em Java, Spring Boot e React',
     github: 'https://github.com/pachecoCaua'  
   },
   {
     nome: 'Daniel Lopes',
     imagem: '/imagens/daniel.jpg',
-    descricao: '.',
+    descricao: 'Me chamo Daniel Lopes, tenho 26 anos e estou em formação como desenvolvedor Full Stack, com foco em Back e Front-End.',
     github: 'https://github.com/Danzete'
   },
   {
@@ -26,19 +26,19 @@ const Grupo3 = [
   {
     nome: 'Rayca Thais',
     imagem: '/imagens/rayca.jpg',
-    descricao: '',
+    descricao: 'Meu nome é Rayca Thais, tenho 18 anos e sou residente em Teresópolis – RJ. Recém-formada no ensino médio, iniciei minha trajetória na área de tecnologia por da residência do Serratec, onde tive meu primeiro contato com programação.Neste projeto, fui responsável por desenvolver a página do carrinho e implementar toda a lógica de funcionamento dele.',
     github: 'https://github.com/raycaThais'
   },
   {
     nome: 'Rodrigo Schuab',
     imagem: '/imagens/rodrigo.jpg',
-    descricao: 'Sou o Rodrigo, tenho 25 anos, o Serratec é meu primeiro contato de verdade no mundo da TI. E nesse trabalho fui responsável pela página Sobre do Grupo 3.',
+    descricao: 'Sou o Rodrigo, tenho 25 anos, estou em formação como Desenvolvedor Full Stack e cursando Análise e Desenvolvimento de Sistemas. E nesse trabalho fui responsável pela página Sobre do Grupo 3.',
     github: 'https://github.com/rodschuab'
   },
   {
     nome: 'Sabrina Siqueira',
     imagem: '/imagens/sabrina.jpg',
-    descricao: '',
+    descricao: 'Meu nome é Sabrina Siqueira, sou estudante de Sistemas de Informação no CEFET/RJ. Sou apaixonada por tecnologia, buscando constantemente aprender e me desenvolver em programação e soluções digitais. Participo de projetos acadêmicos e pessoais voltados à inovação, e tenho interesse especial em desenvolvimento web, aplicações mobile e sistemas voltados para otimização de processos.',
     github: 'https://github.com/Sai-czs'
   },
 ];
